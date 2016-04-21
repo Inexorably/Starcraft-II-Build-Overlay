35a450a1a61162e4c3b59efbcb90866f48d8f157
Inexorably <shawnsaxeysantillo@gmail.com>
2016-04-20 18:01:52 -0700

    Added files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include "Windows.h"
+#include <QDebug>
+#include <QTimer>
+
+MainWindow::MainWindow(QWidget *parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+  setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+  setAttribute(Qt::WA_TranslucentBackground);
+  // example of target window class: "Notepad++"
+  target_window = FindWindowA("Notepad", 0);
+  if (!target_window) {
+    qDebug() << "window not found";
+    return;
+  }
+  QTimer* timer = new QTimer(this);
+  connect(timer, SIGNAL(timeout()), this, SLOT(update_pos()));
+  timer->start(50); // update interval in milliseconds
+}
+
+MainWindow::~MainWindow() {
+  delete ui;
+}
+
+void MainWindow::update_pos() {
+  RECT rect;
+  if (GetWindowRect(target_window, &rect)) {
+    setGeometry(rect.left, rect.top, rect.right - rect.left,
+                rect.bottom - rect.top);
+  } else {
+    //maybe window was closed
+    qDebug() << "GetWindowRect failed";
+    QApplication::quit();
+  }
+  emit true;
+}
---
+++ mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    HWND target_window;
+
+private slots:
+    void update_pos();
+};
+
+#endif // MAINWINDOW_H
